13b9b06d33da8cb63f644079879d2512137bbeb4
njmercado <njmercado@uninorte.edu.co>
2020-10-29 19:46:07 -0500

    Componente creado para la selección de opciones. No se usó el de vuetify ya que se necesitaba un mayor grado de configuración, en cuanto a estilos

---
+++ components/Chooser.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-col id="chooser">
+    <v-row id="chooser-input">
+      <input v-model="chosenItem" type="text" :placeholder="chosenItem" readonly />
+      <v-btn id="chooser-icon" @click="showOptions = !showOptions">
+        <v-icon id="icon" color="white"
+          >mdi-arrow-down-drop-circle-outline</v-icon
+        >
+      </v-btn>
+    </v-row>
+    <v-row>
+      <v-col v-if="showOptions" id="items">
+        <div id="items-container">
+          <v-row id="item" @click="chosenItem = item; showOptions = !showOptions" v-for="(item, index) in items" :key="index">{{
+            item
+          }}</v-row>
+        </div>
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    chosenItem: '',
+    showOptions: false,
+  }),
+  props: { items: { type: Array }, label: {type: String} },
+}
+</script>
+
+<style>
+#chooser {
+  position: relative;
+}
+
+#chooser-input {
+  background-color: white;
+  border-radius: 16px;
+  position: relative;
+}
+#chooser-input input {
+  padding: 8px 10px;
+  width: 90%;
+}
+
+#chooser-icon {
+  background-color: #e07777;
+  width: 10%;
+  height: 100%;
+  border-radius: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  position: absolute;
+  right: 0;
+}
+
+#items {
+  position: absolute;
+}
+#items {
+  background-color: white;
+  margin-top: 16px;
+  left: 0;
+  border-radius: 16px;
+}
+#items-container {
+  border-radius: 16px;
+  padding: 16px 24px;
+  max-height: 200px;
+  overflow-y: scroll;
+}
+#item {
+  background-color: #dddee9;
+  border-radius: 8px;
+  padding: 12px;
+  margin-top: 4px;
+  cursor: pointer;
+}
+#item:hover {
+  background-color: lightpink;
+  color: white;
+}
+</style>
