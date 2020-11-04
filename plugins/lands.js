535012eac100418932714891b6c58823f95e8e8e
njmercado <njmercado@uninorte.edu.co>
2020-11-03 23:34:31 -0500

    Información de prueba a mostrar

---
+++ plugins/lands.js
@@ -0,0 +1,407 @@
+const states = [
+  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Atlantico',
+    size: 'small',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Cundidamarca',
+    size: 'big',
+    population: '10000000',
+    cases: "5000000",
+    childs: [//ciites
+      {
+        name: 'Bogotá',
+        size: 'big',
+        population: '5000000',
+        cases: "510000",
+        childs: [ //neightborhoods
+          {
+            name: 'Ciudad Bolivar',
+            size: 'medium',
+            population: '1000000',
+            cases: "10000",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },  {
+    name: 'Antioquia',
+    size: 'medium',
+    population: '1000000',
+    cases: "100000",
+    childs: [//ciites
+      {
+        name: 'Barranquilla',
+        size: 'big',
+        population: '500000',
+        cases: "50000",
+        childs: [//neightborhods
+          {
+            name: 'Simon Bolivar',
+            size: 'big',
+            population: '100000',
+            cases: "5000",
+          },
+        ],
+      },
+      {
+        name: 'Soledad',
+        size: 'medium',
+        population: '200000',
+        childs: [
+          {
+            name: 'Las Trinitarias',
+            size: 'small',
+            population: '20000',
+            cases: "1000",
+          },
+        ],
+      },
+    ],
+  },
+]
+
+module.exports = {
+  states
+}
