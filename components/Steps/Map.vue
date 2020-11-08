96f580c673f29238e34a7961544e18c26f35f9a7
njmercado <njmercado@uninorte.edu.co>
2020-11-07 23:06:06 -0500

    Ahora el mapa de colombia será mostrado en vez de los circulos que antes se tenian.

--- components/Steps/Map.vue
+++ components/Steps/Map.vue
@@ -1,26 +1,56 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col
-      xs="4"
-      sm="3"
-      md="2"
-      lg="2"
-      xl="2"
-      justify="center"
-      align="center"
-      v-for="(item, index) in chosenItem"
-      :key="index"
-    >
-      <div @click="setData(item)" :class="getClasses(item.size)">
-        <MapItem
-          :name="item.name"
-          :cases="item.cases"
-          :population="item.population"
-          @click="setData(item)"
-        ></MapItem>
-      </div>
-    </v-col>
-  </v-row>
+  <div class="px-8">
+    <div v-if="!chosenState">
+      <v-img src="map/colombia.png" width="512px">
+        <v-tooltip
+          v-for="(item, index) in chosenItem"
+          :key="index"
+          top
+          style="padding: 0px; opacity: 1"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <div
+              :style="item.image.style"
+              :class="item.image.class + ' position-point'"
+              v-on="on"
+              v-bind="attrs"
+              @click="setData(item)"
+            ></div>
+          </template>
+          <div
+            id="template-tooltip"
+            class="black--text py-4 px-4 rounded"
+            style="background-color: white; margin: -10px -20px"
+          >
+            <div>{{ item.name }}</div>
+            <div>Numero de casos:</div>
+            <div>{{ item.cases }}</div>
+          </div>
+        </v-tooltip>
+      </v-img>
+    </div>
+    <v-row v-else justify="center" align="center">
+      <v-col
+        xs="4"
+        sm="3"
+        md="2"
+        lg="2"
+        xl="2"
+        justify="center"
+        align="center"
+        v-for="(item, index) in chosenItem"
+        :key="index"
+      >
+        <div @click="setData(item)">
+          <MapItem
+            :name="item.name"
+            :cases="item.cases"
+            :population="item.population"
+          ></MapItem>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -31,19 +61,20 @@ export default {
   data: () => ({
     chosenItem: {},
     dataIndex: 0,
+    chosenState: false,
   }),
   mounted() {
     this.chosenItem = states
   },
   methods: {
     setData(item) {
-      if(this.dataIndex === 0) this.$store.commit('filter/filterState', item.name)
-      if(this.dataIndex === 1) this.$store.commit('filter/town', item.name)
-      if(this.dataIndex === 2) this.$store.commit('filter/neighbohood', item.name)
+      if (this.dataIndex === 0)
+        this.$store.commit('filter/filterState', item.name)
+      if (this.dataIndex === 1) this.$store.commit('filter/town', item.name)
+      if (this.dataIndex === 2)
+        this.$store.commit('filter/neighbohood', item.name)
       this.chosenItem = item.childs
-    },
-    getClasses(size) {
-      return `circle ${size}-circle`
+      this.chosenState = true
     },
   },
   components: {
@@ -53,21 +84,41 @@ export default {
 </script>
 
 <style>
-.circle {
-  border-radius: 100%;
+.relative {
+  position: relative;
+}
+.absolute {
+  position: absolute;
+}
+
+.z-index-1 {
+  z-index: 1;
+}
+.z-index-2 {
+  z-index: 2;
+}
+.z-index-3 {
+  z-index: 3;
+}
+.z-index-4 {
+  z-index: 4;
+}
+
+.state-image {
+  position: absolute;
+}
+
+.position-point {
+  width: 10px;
+  height: 10px;
   background-color: white;
-  margin: 10px;
+  z-index: 10;
+  border-radius: 50%;
+  transition-property: width height;
+  transition-duration: 0.5s;
 }
-.small-circle {
-  width: calc(50px + 4vw);
-  height: calc(50px + 4vw);
-}
-.medium-circle {
-  width: calc(75px + 6vw);
-  height: calc(75px + 6vw);
-}
-.big-circle {
-  width: calc(100px + 8vw);
-  height: calc(100px + 8vw);
+.position-point:hover {
+  width: 16px;
+  height: 16px;
 }
 </style>
