612dc70892de5b3c4340b7b1aa53286773792ac6
njmercado <njmercado@uninorte.edu.co>
2020-11-11 19:52:17 -0500

    Agregrados filtros para seleccionar que se va a hacer con la información escogida

---
+++ components/Steps/KindOfFilter.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row justify="center">
+    <v-col xs="8" sm="8" md="8" lg="8">
+      <Chooser @chosen="chosenItem" :items="items" label="filtro"></Chooser>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Chooser from '../Chooser'
+export default {
+  data: () => ({
+    items: [
+      'Sexo',
+      'Edad',
+      'Escolaridad',
+      'Estado Civil',
+      'Arma',
+      'Clase Empleado'
+    ],
+  }),
+  methods: {
+    chosenItem(val) {
+      this.$emit('filter', val)
+    }
+  },
+  components: {
+    Chooser,
+  },
+}
+</script>
---
+++ components/Steps/ShowByFilter.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row justify="center">
+    <v-col xs="8" sm="8" md="8" lg="8">
+      <Chooser @chosen="chosenItem" :items="items" label="filtro"></Chooser>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Chooser from '../Chooser'
+export default {
+  data: () => ({
+    items: [
+      'Dia',
+      'Mes',
+      'Sexo'
+    ],
+  }),
+  methods: {
+    chosenItem(val) {
+      this.$emit('show', val)
+    }
+  },
+  components: {
+    Chooser,
+  },
+}
+</script>
