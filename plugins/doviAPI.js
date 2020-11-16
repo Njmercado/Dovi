7e8a38e7d513843b61388b78f4b2796f73956232
njmercado <njmercado@uninorte.edu.co>
2020-11-16 14:25:43 -0500

    Eliminadas funciones innecesarias

--- plugins/doviAPI.js
+++ plugins/doviAPI.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
-let url_dev = "http://localhost:5000"
+// let url_dev = "http://localhost:5000"
+let url_dev = "https://dovi-api.herokuapp.com"
 // const heroku_url = "https://dovi-api.herokuapp.com"
 // const aws_url = "http://54.224.32.155:5000"
 
@@ -17,42 +18,7 @@ async function getCasesByCountry(showBy) {
     return result
 }
 
-async function getCasesByState(state, filter, filterValue, showBy) {
-    state = state.toUpperCase()
-
-    if(showBy === 'DayName' && filter === '' && state === '' && town === '' && filterValue === '') showBy = 'dayname'
-
-    filter = filter.length != 0 ? `/by${filter}` : ''
-    showBy = showBy.length != 0 ? `_by${showBy}` : ''
-    state = state.length != 0 ? `/${state}` : ''
-    filterValue = filterValue.length != 0 ? `/${filterValue}` : ''
-
-    const endpoint = `${filter}${showBy}${state}${filterValue}` 
-    const url = `${url_dev}${endpoint}`
-    const result = await axios.get(url)
-    return result
-}
-
-async function getCasesByTown(state, town, filter, filterValue, showBy) {
-    state = state.toUpperCase()
-    town = town.toUpperCase()
-
-    if(showBy === 'DayName' && filter === '' && state === '' && town === '' && filterValue === '') showBy = 'dayname'
-
-    filter = filter.length != 0 ? `/by${filter}` : ''
-    showBy= showBy.length != 0 ? `_by${showBy}` : ''
-    state = state.length != 0 ? `/${state}` : ''
-    town = town.length != 0 ? `/${town}` : ''
-    filterValue = filterValue.length != 0 ? `/${filterValue}` : ''
-
-    const endpoint = `${filter}${showBy}${state}${town}${filterValue}` 
-    // const endpoint = `${url_dev}/`
-    const url = `${url_dev}${endpoint}`
-    const result = await axios.get(url)
-    return result
-}
-
-async function getCasesByNeighborhood(state, town, neighborhood, filter, filterValue, showBy) {
+async function getCasesBy(state, town, neighborhood, filter, filterValue, showBy) {
 
     if(showBy === 'DayName' && filter === '' && state === '' && town === '' && filterValue === '') showBy = 'dayname'
 
@@ -87,21 +53,15 @@ async function apiHandler({place, filter, showBy="Sex"}) {
     const filterName = capitalize(chosenKey[0]) || ''
     const filterValue = filter[chosenKey[0]] || ''
 
-    if(neighborhood.length > 0)
-        return await getCasesByNeighborhood(state, town, neighborhood, filterName, filterValue, showBy)
-    if(town.length > 0)
-        return await getCasesByTown(state, town, filterName, filterValue, showBy)
-    if(state.length > 0)
-        return await getCasesByState(state, filterName, filterValue, showBy)
-    else
+    if(neighborhood === '' && town === '' && state === '')
         return await getCasesByCountry(showBy)
+    else
+        return await getCasesBy(state, town, neighborhood, filterName, filterValue, showBy)
 }
 
 module.exports = {
     allCases: getAllCases,
     apiHandler: apiHandler,
     casesByCountry: getCasesByCountry,
-    casesByState: getCasesByState,
-    casesByTown: getCasesByTown,
-    getCasesByNeighborhood: getCasesByNeighborhood,
+    getCasesBy: getCasesBy,
 }
