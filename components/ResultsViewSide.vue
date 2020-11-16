b351613af9bb344b27c8508d5f4e5ab34ae2dafb
njmercado <njmercado@uninorte.edu.co>
2020-11-16 00:32:08 -0500

    Código formateado y agregados nuevas Graficas

--- components/ResultsViewSide.vue
+++ components/ResultsViewSide.vue
@@ -1,27 +1,37 @@
 <template>
   <div>
     <v-row justify="space-between" align="center" class="px-8">
-      <div class="text-h3" :style="{ color: $vuetify.theme.themes['light'].background }">Dovi</div>
+      <v-img src="logo.png" max-width="20%" contain></v-img>
       <div>
         <v-btn :color="$vuetify.theme.themes['light'].background" class="text-lowercase white--text" rounded>predecir</v-btn>
-        <v-btn :color="$vuetify.theme.themes['light'].background" class="text-lowercase white--text" rounded >información</v-btn>
+        <v-btn
+          :color="$vuetify.theme.themes['light'].background"
+          class="text-lowercase white--text"
+          rounded @click="openInfoModal = !openInfoModal"
+        >sobre nosotros</v-btn>
       </div>
     </v-row>
     <v-row justify="center" align="center" style="min-height: 85vh">
-      <!-- <LineChart></LineChart> -->
-      <DonutChart v-if="loaded" :chartData="chartdata"></DonutChart>
+      <LineChart v-if="loaded && chart === 'line'" :chartData="chartdata"></LineChart>
+      <BarChart v-if="loaded && chart === 'bar'" :chartData="chartdata"></BarChart>
+      <DonutChart v-if="loaded && chart === 'donut'" :chartData="chartdata"></DonutChart>
     </v-row>
+    <SidePanel :open="openInfoModal"></SidePanel>
   </div>
 </template>
 
 <script>
 import LineChart from './charts/LineChart'
+import BarChart from './charts/BarChart'
 import DonutChart from './charts/DonutChart'
-import { allCases } from '../plugins/doviAPI'
+import { casesByCountry } from '../plugins/doviAPI'
+import SidePanel from '../components/SidePanel'
 
 export default {
   data: () => ({
+    openInfoModal: false,
     loaded: false,
+    chart: 'donut',
     chartdata: {
       labels: [],
       datasets: [
@@ -43,13 +53,15 @@ export default {
     }
   },
   async mounted(){
-    const result = await allCases()
+    const result = await casesByCountry('Sex')
     this.resetChartData(result.data)
     this.loaded = true
   },
   methods: {
     resetChartData(data) {
-      console.log(data)
+      if(data[0].length >= 5) this.chart = 'bar'
+      if(data[0].length >= 15) this.chart = 'line'
+      else this.chart = 'donut'
       this.chartdata.labels = data[0] 
       this.chartdata.datasets[0].data = data[1] 
     }
@@ -57,6 +69,7 @@ export default {
   components: {
     LineChart,
     DonutChart,
+    BarChart,
   },
 }
 </script>
