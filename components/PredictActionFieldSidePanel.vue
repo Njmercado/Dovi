611cebd0160bf2ee754604a74be79d82b810680d
njmercado <njmercado@uninorte.edu.co>
2020-11-21 16:35:24 -0500

    Agregado selector del campo a predecir. Este selector se muestra antes de los inputs

--- components/PredictActionFieldSidePanel.vue
+++ components/PredictActionFieldSidePanel.vue
@@ -1,35 +1,41 @@
 <template>
   <div>
-    <v-navigation-drawer
-      id="drawer"
-      right
-      absolute
-      temporary
-      v-model="openNavigation"
-      :color="$vuetify.theme.themes['light'].secondary"
-    >
-      <div
-        style="text-align: left"
-        class="text-h4 font-weight-bold white--text"
-      >
-        Predecir Campo de acción
-      </div>
-      <div style="margin-top: 10px">
-        <Age :only="true" @begin="setAge"></Age>
-        <Sex @chosen="setSex"></Sex>
-        <CivilStatus @chosen="setCivilStatus"></CivilStatus>
-        <EmployeeType @chosen="setEmployeeType"></EmployeeType>
-        <Scholarity @chosen="setScholarity"></Scholarity>
-      </div>
-      <v-row justify="center" class="mt-14">
-        <v-btn
-          :color="$vuetify.theme.themes['light'].background"
-          class="text-lowercase white--text"
-          @click="predict"
-          rounded
-          >predecir</v-btn
-        >
+    <v-navigation-drawer id="drawer" right absolute temporary v-model="openNavigation" :color="$vuetify.theme.themes['light'].secondary">
+      <div v-if="!showFields" style="text-align: left"  class="text-h4 font-weight-bold white--text"> Selecciona el campo a predecir </div>
+      <!-- Choosing field to predict -->
+      <v-row v-if="!showFields" justify="center" align="center" style="height: 90%">
+        <v-radio-group v-model="chosenFieldToPredict">
+          <v-radio v-for="(item, index) in listOfFields" :key="index" :value="item">
+            <template v-slot:label>
+              <div class="white--text text-h5 font-weight-bold">{{item}}</div>
+            </template>
+          </v-radio>
+        </v-radio-group>
       </v-row>
+      <!-- Fields to predict another one -->
+      <div v-if="showFields">
+        <div style="text-align: left"  class="text-h4 font-weight-bold white--text"> Predecir {{chosenFieldToPredict}}</div>
+        <v-col style="margin-top: 10px">
+          <Age v-if="chosenFieldToPredict !== 'Edad'" :only="true" @begin="setAge"></Age>
+          <Sex v-if="chosenFieldToPredict !== 'Sexo'" @chosen="setSex"></Sex>
+          <CivilStatus v-if="chosenFieldToPredict !== 'Estado Civil'" @chosen="setCivilStatus"></CivilStatus>
+          <EmployeeType v-if="chosenFieldToPredict !== 'Tipo Empleado'" @chosen="setEmployeeType"></EmployeeType>
+          <Scholarity v-if="chosenFieldToPredict !== 'Escolaridad'" @chosen="setScholarity"></Scholarity>
+        </v-col>
+        <!-- Action buttons -->
+        <v-row justify="center" class="mt-14">
+          <v-btn
+            :color="$vuetify.theme.themes['light'].background"
+            class="text-lowercase white--text"
+            @click="returnToChoseField"
+            rounded>regresar</v-btn>
+          <v-btn
+            :color="$vuetify.theme.themes['light'].background"
+            class="text-lowercase white--text ml-5"
+            @click="predict"
+            rounded>predecir</v-btn>
+        </v-row>
+      </div>
     </v-navigation-drawer>
     <PredictFieldInfoModal
       :open="openModal"
@@ -50,10 +56,19 @@ import PredictFieldInfoModal from './PredictFieldInfoModal'
 
 export default {
   data: () => ({
+    chosenFieldToPredict: '',
+    showFields: false,
+    listOfFields: [
+      "Edad",
+      "Sexo",
+      "Estado Civil",
+      "Tipo Empleado",
+      "Escolaridad",
+    ],
     openModal: false,
     modalData: '',
     openNavigation: false,
-    age: 18,
+    age: -1,
     sex: '-',
     civilStatus: '-',
     scholarity: '-',
@@ -91,11 +106,18 @@ export default {
       this.modalData = result.data
       this.openModal = !this.openModal
     },
+    returnToChoseField() {
+      this.showFields = false
+    }
   },
   watch: {
     open(val) {
       this.openNavigation = this.openNavigation ? this.openNavigation : true
     },
+    chosenFieldToPredict(val) {
+      if(!this.showFields)
+        this.showFields = true
+    }
   },
 }
 </script>
