dc5b3c44734f96715ba95dd4446efac896add2c6
njmercado <njmercado@uninorte.edu.co>
2020-11-21 19:24:39 -0500

    Creado modal para mostrar el resultado de la predicción de violencia intrafamiliar.

---
+++ components/PredictDomesticViolenceInfoModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-dialog max-width='50em' v-model="realModalOpener">
+    <v-card>
+      <v-card-title>
+        <v-row justify='center'>
+          <div id='titulo' style="font-size: 1.2em">
+            Resultado
+          </div>
+        </v-row>
+      </v-card-title>
+      <v-card-text>
+        Usando los filtros dados, podemos establecer que con un porcentaje de <strong>{{data}}</strong>, 
+        podrías ser victima de violencia intrafamiliar.
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: [
+    'open',
+    'data'
+  ],
+  data: () => ({
+    realModalOpener: false
+  }),
+  watch: {
+    open (val) {
+      console.log("open modal")
+      this.realModalOpener = this.realModalOpener ? this.realModalOpener : true
+    }
+  }
+}
+</script>
